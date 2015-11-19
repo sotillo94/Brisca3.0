835ce0669bf3e061abe81c0dae2423601cef9300
jaime <jaimesanzherrero@gmail.com>
2015-11-19 16:54:19 +0100

    imagenes

--- app/src/main/java/com/brisca/moviles/uva/brisca30/Interfaz.java
+++ app/src/main/java/com/brisca/moviles/uva/brisca30/Interfaz.java
@@ -1,11 +1,7 @@
 package com.brisca.moviles.uva.brisca30;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 /**
  * Created by argv0 on 19/11/2015.
@@ -15,7 +11,7 @@ public class Interfaz extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interfaz_juego);
+        setContentView(R.layout.activity_interfaz_juego_bak);
 
     }
 
