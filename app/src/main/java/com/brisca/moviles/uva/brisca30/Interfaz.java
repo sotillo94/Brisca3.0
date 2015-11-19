c53321ff3921ff0b3890f253eb56c9252f205e66
jaime <jaimesanzherrero@gmail.com>
2015-11-19 17:14:26 +0100

    cartas

--- app/src/main/java/com/brisca/moviles/uva/brisca30/Interfaz.java
+++ app/src/main/java/com/brisca/moviles/uva/brisca30/Interfaz.java
@@ -11,7 +11,7 @@ public class Interfaz extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interfaz_juego);
+        setContentView(R.layout.activity_interfaz_juego2);
 
     }
 
