7b5c0e0bf74f2f38faeb01dfa465118f1d09323a
jaime <jaimesanzherrero@gmail.com>
2015-11-20 09:42:02 +0100

    comentarios

--- app/src/main/java/com/brisca/moviles/uva/brisca30/InstruccionesBrisca.java
+++ app/src/main/java/com/brisca/moviles/uva/brisca30/InstruccionesBrisca.java
@@ -4,7 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+/**
+ * Activity para la mostrar las intrucciones.
+ *
+ * Esta actividad sirve para mostrar las instrucciones del juego.
+ *
+ */
 public class InstruccionesBrisca extends AppCompatActivity {
 
     @Override
@@ -12,5 +17,4 @@ public class InstruccionesBrisca extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones_brisca);
     }
-
 }
--- app/src/main/java/com/brisca/moviles/uva/brisca30/Interfaz.java
+++ app/src/main/java/com/brisca/moviles/uva/brisca30/Interfaz.java
@@ -2,20 +2,15 @@ package com.brisca.moviles.uva.brisca30;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
 
 /**
- * Created by argv0 on 19/11/2015.
+ * Activity que muestra la interfaz principal del juego.
  */
 public class Interfaz extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interfaz_juego2);
-
+        setContentView(R.layout.activity_interfaz_juego);
     }
-
-
 }
