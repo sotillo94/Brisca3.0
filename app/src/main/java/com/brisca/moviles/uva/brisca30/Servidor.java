9316b2f17269fc43dfb06b5462362916e56170a8
sotillo94 <sotillo_94@hotmail.es>
2015-12-28 14:33:40 +0100

    codigo de cliente y servidor separado

--- app/src/main/java/com/brisca/moviles/uva/brisca30/Servidor.java
+++ app/src/main/java/com/brisca/moviles/uva/brisca30/Servidor.java
@@ -71,6 +71,7 @@ public class Servidor extends AppCompatActivity implements View.OnClickListener 
             }
         };
 
+
         //inicializamos el chat
         mConnection = new ChatConnection(mUpdateHandler);
         //inicializamos la conexion
